819ec1ef7455e4f57dc73a4a4f33b7aba9c18289
Alan <mago7410alan@gmail.com>
2023-02-01 10:15:31 -0600

    first commit

---
+++ arrowfunction.js
@@ -0,0 +1,54 @@
+
+// Arrow FUnction
+suma = (a,b) => {
+    return a+b;
+}
+suma(5,8);
+resta = (a,b) => {
+    return a-b;
+}
+resta(8,90);
+multi = (a,b) => {
+    return a*b;
+}
+multi(7,1);
+division = (a,b) => {
+    return a/b;
+}
+division(10,2);
+areacuadrado = (a) => {
+    return a*a;
+}
+areacuadrado(5);
+
+// variables locales
+
+function hola(){
+    saludo = "Hola buenos dias";
+    return saludo;
+}
+hola();
+
+function adios(){
+    bye = "Hasta luego regresa pronto";
+    return bye;
+}
+adios();
+
+// variables  Globales
+
+var react = "React es el mejor framework";
+console.log(react);
+
+var js = "javascript es un lenguaje para el frontend";
+console.log(js);
+
+var laUTVT = "es la uni a la que voy :v";
+console.log(laUTVT);
+
+var futbol = "se necesitan 11 jugadores"; 
+console.log(futbol);
+
+
+
+
---
+++ primero.js
@@ -0,0 +1,24 @@
+Array
+[1,2,3,4]
+{nombre: 'Alan'}
+
+//la variable toma valor en la memoria pero nose inicializar
+var edad = 30;
+// Se declara la variable
+var edad;
+
+// Inicializacom la variable
+edad = 30;
+
+
+    function miFunction() {
+        return 3;
+    };
+    var mifunction =  function(a,b) {
+        return a+b;
+
+    }
+
+        function saludarEstudiante(ayuda){
+            console.log('hola ${ayuda}')
+        }
